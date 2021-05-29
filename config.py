e4423291d485268348ad027c5ec8fb8b433313ff
wojdob <wojciech.dobrychlop@gmail.com>
2021-05-29 18:59:55 +0200

    Add initial data

---
+++ config.py
@@ -0,0 +1,9 @@
+PROTEINS = [
+ ('ReAV', 'NC_007765.1_373', 'RS_GCF_000092045.1'),
+ ('ReAIV', 'NC_007761.1_1155', 'RS_GCF_000092045.1'),
+ ('EcAI', 'NZ_CP033092.2_2121', 'RS_GCF_003697165.2'),
+ ('EcAII', 'NZ_CP033092.2_841', 'RS_GCF_003697165.2'),
+ ('EcAIII', 'NZ_CP033092.2_3034', 'RS_GCF_003697165.2'),
+]
+
+TAX_LEVELS = ['domain', 'phylum', 'class', 'order', 'family', 'genus', 'species']
