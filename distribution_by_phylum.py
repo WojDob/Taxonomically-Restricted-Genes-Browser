13e92dcf9226f4f44d2b012bbb2461e861ac437c
wojdob <wojciech.dobrychlop@gmail.com>
2021-05-30 16:48:00 +0200

    Add phylum distribution of asparaginases

--- distribution_by_phylum.py
+++ distribution_by_phylum.py
@@ -42,9 +42,26 @@ for genome in metadata:
 
 #Count number of orthologs of each asp. for each phylum
     #Create phylum_count dict
+phylum_count = dict()
         #Add each phylum as key, dict of count of 0 of every asp. as value
+for phylum in phylums:
+    phylum_count[phylum] = {
+        'ReAV':0,
+        'ReAIV':0,
+        'EcAI':0,
+        'EcAII':0,
+        'EcAIII':0,
+    }
+
     #Load emboss-pfam-rbh.json to huge_dictionary
-    #For asparaginase in asparaginases_list
-        #for genome in huge_dictionary[asparaginase]
-            #genomes_origin = origin[genome.key?]
-            #phylum_count[genomes_origin] += genome.value?
+with open('data/emboss-pfam-rbh.json','r') as json_file:
+    orthologs_data = json.load(json_file)
+
+for asparaginase in ASPARAGINASES:
+    for genome in orthologs_data[asparaginase]:
+        genomes_origin = origin_of_genomes[genome]
+        phylum_count[genomes_origin][asparaginase] += len(orthologs_data[asparaginase][genome])
+
+
+with open('data/phylum_distribution.json', 'w') as f:
+    json.dump(phylum_count, f, indent=4)
