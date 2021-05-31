031c557d1bc0b811e57fca64f01fd8b85ce759f0
wojdob <wojciech.dobrychlop@gmail.com>
2021-05-31 18:20:13 +0200

    Order phylums the same way in both txt files

---
+++ count_classification_data.py
@@ -0,0 +1,15 @@
+'''
+CREATED: 
+    2021-05-29
+
+
+USAGE:
+    Count how many of each differnt phylums, families etc. of bacteria there are
+    The end result should return about 30 thousand different species'
+
+
+USED DATA FILES:
+    data/metadata.json
+    Contains data about classification of each bacteria 
+
+'''
