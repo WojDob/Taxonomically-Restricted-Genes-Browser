26566757ccd728b36aa5510923686877dc2219e1
wojdob <wojciech.dobrychlop@gmail.com>
2021-09-05 17:46:46 +0200

    Add basic search functionality

--- specific_gene_browser/ui/urls.py
+++ specific_gene_browser/ui/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home')
+    path('', views.home, name='home'),
+    path('search_results', views.search_results, name='search-results'),
 ]
--- specific_gene_browser/ui/views.py
+++ specific_gene_browser/ui/views.py
@@ -1,4 +1,20 @@
+from browser.models import Taxon
+from django.db.models.query_utils import Q
 from django.views.generic import TemplateView
+from django.shortcuts import render
 
-class HomeView(TemplateView):
-    template_name = "ui/home.html"
+
+def home(request):
+    context = dict()
+    return render(request, 'ui/home.html', context)
+
+
+def search_results(request):
+    context = dict()
+    if request.method == "POST":
+        searched = request.POST.get("searched")
+        results = Taxon.objects.filter(name__icontains=searched)
+        context = {"searched": searched, "results": results}
+        return render(request, 'ui/search_results.html', context)
+    else:
+        return render(request, 'ui/search_results.html', context)
