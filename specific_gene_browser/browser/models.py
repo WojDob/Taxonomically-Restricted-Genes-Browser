1584343d6a95592d538878cb34688f2a242562a4
wojdob <wojciech.dobrychlop@gmail.com>
2021-10-02 14:27:52 +0200

    Change data models

--- specific_gene_browser/browser/models.py
+++ specific_gene_browser/browser/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class TaxonomicUnit(models.Model):
     name = models.CharField(max_length=30, unique=True)
@@ -13,6 +13,7 @@ class TaxonomicUnit(models.Model):
 
 class Taxon(models.Model):
     name = models.CharField(max_length=250)
+    accession = models.CharField(max_length=50)
     taxonomic_unit = models.ForeignKey(
         "TaxonomicUnit", on_delete=models.CASCADE, related_name="taxons", db_index=True
     )
@@ -20,8 +21,13 @@ class Taxon(models.Model):
         "self", on_delete=models.CASCADE, null=True, related_name="child_taxons", db_index=True
     )
 
-    def __str__(self):
-        return "({}) {}".format(self.taxonomic_unit.name,self.name)
+    # Genome related fields
+    protein_number = models.PositiveIntegerField(null=True, blank=True)
+    species_isolation_index = models.FloatField(null=True, blank=True)
+    genus_isolation_index = models.FloatField(null=True, blank=True)
+
+    class Meta:
+        ordering = ['taxonomic_unit']
 
     def print_whole_classification(self):
         parent = self
@@ -36,13 +42,16 @@ class Taxon(models.Model):
             classification_list.append(parent)
             parent = parent.parent
         return classification_list
+    
+    def __str__(self):
+        return "({}) {}".format(self.taxonomic_unit.name, self.name)
 
 
-class SpecificGene(models.Model):
-    name = models.CharField(max_length=250)
-    specific_to = models.ForeignKey(
-        "Taxon", on_delete=models.CASCADE, related_name="specific_genes"
+class TaxonomicallyRestrictedGene(models.Model):
+    accession = models.CharField(max_length=50)
+    origin_genome = models.ForeignKey(
+        "Taxon", on_delete=models.CASCADE, related_name="taxonomically_restricted_genes"
     )
 
     def __str__(self):
-        return self.name
+        return self.accession
